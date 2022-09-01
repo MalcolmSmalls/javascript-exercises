bb51a97d24f94d7b1e4134c5c2c53768caf63c50
Malcolm Smalls <smallsbeats@gmail.com>
2022-08-31 21:24:45 -0700

    Finish 05 sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,18 @@
-const sumAll = function() {
-
-};
+const sumAll = function(startingNum, sumUpToNum) {
+    let total = 0
+    if(typeof(startingNum) !== "number" || typeof(sumUpToNum) !== "number"){
+        return 'ERROR'
+    }else if(startingNum > sumUpToNum){
+        for(i=sumUpToNum;i<=startingNum;i++){
+            total += i} 
+    }else if(startingNum < sumUpToNum && startingNum > 0){
+        for(i=startingNum;i<=sumUpToNum;i++){
+            total += i}   
+    }else{
+            return 'ERROR'
+    }
+        return total 
+}
 
 // Do not edit below this line
 module.exports = sumAll;
