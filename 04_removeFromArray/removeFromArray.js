7811a0f071b5f3d00dde8402309f6df18644a2a3
Malcolm Smalls <smallsbeats@gmail.com>
2022-08-31 20:43:00 -0700

    Finish removeFromArray.js

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,12 @@
-const removeFromArray = function() {
-
-};
+const removeFromArray = function(array, ...removedItems) {
+    newArr = []
+    array.forEach(item => {
+      if(removedItems.includes(item) === false){
+        newArr.push(item)
+      }
+    })
+    return newArr
+  };
 
 // Do not edit below this line
 module.exports = removeFromArray;
